7e5b3c4a55d026725b5ea1c1b6d3abf8929e1569
HarveyBong <felix.nagy@chasacademy.se>
2024-09-19 12:51:42 +0200

    Added  PWA to application

--- u07-weather-app/vite.config.ts
+++ u07-weather-app/vite.config.ts
@@ -1,7 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'U07 Weather app giga lord',
+        short_name: 'U07 Weather app',
+        display: 'fullscreen',
+        display_override: ["fullscreen", "minimal-ui"],
+       
+        
+       
+     theme_color: "#393E41",
+       //background_color:'#393E41',
+        icons: [
+            {
+                src: 'pwa-64x64.png',
+                sizes: '64x64',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any'
+            },
+            {
+                src: 'maskable-icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable'
+            }
+        ],
+      }, 
+    })
+  ],
 })
